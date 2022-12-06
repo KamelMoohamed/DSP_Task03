29cd9e5a203b34b5435f6aa7371fe6a190a6ff68
Kamel Mohamed <112772763+Kamel-Mohammed@users.noreply.github.com>
2022-12-06 15:04:08 +0200

    fix: fixing model 1 and model 2 to be 90% accuaracy

--- processing/processing.py
+++ processing/processing.py
@@ -20,19 +20,47 @@ class Processing:
     
         return mfccs_scaled_features
 
-    def predict_pipelines(self, file):
-        song = Song(file)
-        lst1 = [song.max_pitch, song.avg_pitch, song.var_pitch, song.max_lpc, song.avg_lpc, song.var_lpc, 
-                song.harmonic, song.harmonic_var, song.percussive, song.percussive_var, song.chromaAvg, 
-                song.chromaVar, song.chroma_stft_mean, song.chroma_stft_var, song.chroma_cqt_mean, 
-                song.chroma_cqt_var, song.Mfccs, song.Mfccs_var, song.mfcc_delta_mean, 
-                song.mfcc_delta_var, song.Contrast, song.Contrast_var, song.Rolloff, song.Rolloff_var, 
-                song.Zrate, song.Zrate_var, song.Cent, song.Cent_var, song.tonnetz_mean, song.tonnetz_var, 
-                song.poly_features_mean, song.poly_features_var, song.spec_bw_mean, song.spec_bw_var, 
-                song.rmse_mean, song.rmse_var]
+    def features_extractor_lpc(self, file):
+        audio, sample_rate = librosa.load(file, res_type='kaiser_fast') 
+        mfccs_features = librosa.lpc(audio, order=2)
+        mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+        
+        return mfccs_features
+    def features_extractor_rms(self, file):
+        audio, sample_rate = librosa.load(file, res_type='kaiser_fast') 
+        mfccs_features = librosa.feature.rms(audio)
+        mfccs_scaled_features = np.mean(mfccs_features)
 
-        sentenceModelInputs = np.array(lst1).reshape(1,-1)
-        personsModelInputs = np.array(self.features_extractor(file)).reshape(1, -1)
+        return mfccs_scaled_features
+    
+    def features_extractor_plp(self, file):
+        audio, sample_rate = librosa.load(file, res_type='kaiser_fast') 
+        mfccs_features = librosa.beat.plp(audio)
+        mfccs_scaled_features = np.mean(mfccs_features)
+
+        return mfccs_scaled_features
+
+    def predict_pipelines(self, file):
+        # song = Song(file)
+        # lst1 = [song.max_pitch, song.avg_pitch, song.var_pitch, song.max_lpc, song.avg_lpc, song.var_lpc, 
+        #         song.harmonic, song.harmonic_var, song.percussive, song.percussive_var, song.chromaAvg, 
+        #         song.chromaVar, song.chroma_stft_mean, song.chroma_stft_var, song.chroma_cqt_mean, 
+        #         song.chroma_cqt_var, song.Mfccs, song.Mfccs_var, song.mfcc_delta_mean, 
+        #         song.mfcc_delta_var, song.Contrast, song.Contrast_var, song.Rolloff, song.Rolloff_var, 
+        #         song.Zrate, song.Zrate_var, song.Cent, song.Cent_var, song.tonnetz_mean, song.tonnetz_var, 
+        #         song.poly_features_mean, song.poly_features_var, song.spec_bw_mean, song.spec_bw_var, 
+        #         song.rmse_mean, song.rmse_var]
+
+        x = list(self.features_extractor(file))
+        x1 = list(self.features_extractor_lpc(file))
+        x2 = self.features_extractor_rms(file)
+        x3 = self.features_extractor_plp(file)
+        sentenceModelInputs = x + x1
+        sentenceModelInputs.append(x2)
+        sentenceModelInputs.append(x3)
+
+        sentenceModelInputs = np.array(sentenceModelInputs).reshape(1, -1)
+        personsModelInputs = np.array(x).reshape(1, -1)
 
         sentenceModelInputs = self.model1Scaler.transform(sentenceModelInputs)
         personsModelInputs = self.model2Scaler.transform(personsModelInputs)
@@ -41,12 +69,12 @@ class Processing:
         prediction2 = self.model2.predict(np.array(personsModelInputs))
 
         sentence = self.process_output1(prediction1[0])
-        personIndex = 0
         person = self.process_output2(prediction2[0])
+        
         return sentence, person
 
     def process_output1(self, labelIndex):
-        return ["Open", "Close", "Others"][labelIndex]
+        return ["Open", "Others"][labelIndex]
 
     def process_output2(self, labelIndex):
         return ["Abdelrahman", "Kamel", "Sama", "Yousr", "Others"][labelIndex]
